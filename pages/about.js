9b5ac055728bc584c073ac545fd469cb2bbc84a8
Thomas Imensek <thomas.imensek@bergfex.at>
2022-02-01 19:51:25 +0100

    initial commit;

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
+ const About = () => {
   return (
     <div>
-        <h1>hello</h1>
+        <h1>About!</h1>
         <p>
             Bacon ipsum dolor amet tri-tip frankfurter chicken pancetta ball tip tail sirloin cow hamburger drumstick shank bacon pork belly buffalo strip steak. Jerky buffalo leberkas kevin, sirloin pig pork belly burgdoggen meatloaf bresaola fatback. Prosciutto tri-tip frankfurter, alcatra brisket salami ham pig. Sausage shank jowl andouille boudin. Hamburger tail capicola frankfurter chicken ham. Drumstick chicken boudin bacon fatback, pastrami chislic porchetta. Spare ribs kielbasa leberkas turducken bacon short loin brisket picanha ribeye meatball shankle pig.
+
 
             Frankfurter spare ribs landjaeger fatback. Ribeye shank shankle ham pig kevin ground round pork chop hamburger spare ribs tenderloin venison andouille pork loin alcatra. Beef ribs fatback turducken, landjaeger meatball sirloin jerky cupim ground round capicola ham hock pancetta tenderloin. Tail bacon meatloaf capicola burgdoggen, shank boudin.
         </p>
     </div>
-  )
-}
+  );
+ }
+ export default About;
