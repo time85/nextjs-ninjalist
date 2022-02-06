05d5266bc0945dabad7ca61cdeecef47b471dd62
Thomas Imensek <thomas.imensek@bergfex.at>
2022-02-06 20:06:15 +0100

    create getStaticPaths for SSG;

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const Detail = () => {
+
+export const getStaticPaths = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const data = await response.json();
+
+    const paths = data.map(ninja => {
+        return {
+            params: { id: ninja.id.toString()}
+        }
+    });
+
+    return {
+        paths,
+        fallback: false
+    };
+}
+
+
+const Detail = ({}) => {
     return (
         <div>
-            <h1>DetailsPage</h1>
+            <h1>Detail Page for </h1>
         </div>
     );
 };
