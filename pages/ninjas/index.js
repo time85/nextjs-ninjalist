38788214b3771835e382bac5d5099e32eb288f51
Thomas Imensek <thomas.imensek@bergfex.at>
2022-02-06 19:48:29 +0100

    fetch data through getStaticProps

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Head from "next/head";
 
-const Index = () => (
+export const getStaticProps = async () => {
+
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const data = await response.json();
+
+    return {
+        props: {
+            ninjas: data
+        }
+    };
+}
+
+const Index = ({ninjas}) => (
     <>
         <Head>
             <title>NinjaList | NinjaList</title>
@@ -9,9 +21,14 @@ const Index = () => (
         </Head>
         <div>
             <h1>Ninjas INDEX</h1>
-            <p>
-                Ninjagooo
-            </p>
+            {ninjas.map(ninja => (
+                <div key={ninja.id}>
+                    <a>
+                        <h3>{ninja.name}</h3>
+                    </a>
+                </div>
+
+            )  )}
         </div>
     </>
 );
